eb9159d22190026584ceec2474dc214ed832ae09
espona <lucia.espona@gmail.com>
2021-03-06 01:14:21 +0100

    Improved script to generate files and inserts

--- index.js
+++ index.js
@@ -1,67 +1,121 @@
 var faker = require('faker');
+var chalk = require('chalk');
 require('dotenv').config()
-var MongoClient = require('mongodb').MongoClient;
+const {ObjectID} = require('mongodb');
 
-// Connect to the db
-const mongodb_uri = process.env.DB_URI
+// Read input files
+'use strict';
+const fs = require('fs');
 
-MongoClient.connect(mongodb_uri, async function (err, client) {
-    if (err) throw er
+async function run() {
+    console.log(chalk.cyan.bold("\n1. Reading input files:"))
+    var rawdata = fs.readFileSync('./data/input/customer.json');
+    let customers = JSON.parse(rawdata);
+    console.log(" - Read " + customers.length + " customers");
 
-    var db = client.db('data_generation');
+    rawdata = fs.readFileSync('./data/input/employee.json');
+    let employees = JSON.parse(rawdata);
+    console.log(" - Read " + employees.length + " employees");
 
-    const collection = db.collection('timesheets')
+    rawdata = fs.readFileSync('./data/input/project.json');
+    let projects = JSON.parse(rawdata);
+    console.log(" - Read " + projects.length + " projects");
 
-    const numProjects = 100
-    const numEmployees = 50
-    const numServices = 20
-    const numDates = 10
+    rawdata = fs.readFileSync('./data/input/service.json');
+    let services = JSON.parse(rawdata);
+    console.log(" - Read " + services.length + " services");
 
-    // "id": "aaa-bbb-aaa-005", -> MongoDB
 
-    for (let i = 0; i < numProjects; i++) { 
-        // "ref-project": "aaa-aaa-ddd-004",
-        var project = faker.random.uuid()
-        
-        for (let j = 0; j < numEmployees; j++) { 
-            // "ref-employee": "aaa-aaa-bbb-002",
-            var employee = faker.random.uuid()
-    
-            for (let k = 0; k < numServices; k++) { 
-                // "ref-service": "aaa-aaa-aaa-002",
-                var service = faker.random.uuid()
+    // Generate timesheets
+    console.log(chalk.cyan.bold("\n2. Generating time sheets:"))
 
-                for (let l = 0; l < numDates; l++) { 
-                    // "date": "2020-11-01",
-                    // TODO: check not repeating
-                    // TODO: limit period
-                    // TODO: set it to date only
-                    var date = faker.date.past()
+    const numTimesheets = 300
+    const minDates = 10
+    const maxDates = 80
+    const batchSize = 100
 
-                    // "quantity": 1.5
-                    var quantity = faker.random.number() % 8 + (faker.random.number() % 10) / 10.0
+    var timesheet;
+    var timesheets = [];
+    var count = 0
+    var outputFiles = []
 
-                    await collection.insertOne({ 
-                        "ref-project": project, 
-                        "ref-employee": employee,
-                        "ref-service": service, 
-                        date,
-                        quantity
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
+    }
 
-                    });    
+    while ((count + timesheets.length) < numTimesheets) { 
+        var project = projects[getRandomInt(0, projects.length)]._id["$oid"]
+        var employee = employees[getRandomInt(0, employees.length)]._id["$oid"]
+        var service = services[getRandomInt(0, services.length)]._id["$oid"]
 
-                }
+        var numDates = getRandomInt(minDates, maxDates)
+        for (let l = 0; l < numDates; l++) { 
+            // "date": "2020-11-01",
+            // TODO: check not repeating
+            // TODO: limit period
+            // TODO: set it to date only
+            //var date = new Date(faker.date.past().toISOString().slice(0,10));
+            var date = faker.date.past().toISOString().slice(0,10);
+
+            // "quantity": 1.5
+            var quantity = (1 + faker.random.number()) % 8 + (faker.random.number() % 10) / 10.0
+
+            timesheet = { 
+                "ref-project": new ObjectID(project), 
+                "ref-employee": new ObjectID(employee),
+                "ref-service": new ObjectID(service), 
+                date,
+                quantity
+            }    
+            timesheets.push(timesheet)
+
+            // save one batch to file and end the process
+            if ((timesheets.length >= batchSize) || ((count + timesheets.length) >= numTimesheets)){
+                count += timesheets.length
+                var outputPath = './data/output/timesheet_' + count + '.json'
+                fs.writeFileSync(outputPath, JSON.stringify(timesheets, null, 2))
+                outputFiles.push(outputPath)
+                timesheets = []
+                console.log(chalk.yellow.italic("* Saved batch to: " + outputPath))
+                if (count >= numTimesheets) break
             }
         }
-    }
-     
-    
-    collection.countDocuments(function (err3, count) {
-        if (err3) throw err3;
         
-        console.log('Total Rows: ' + count);
-    });
+        if (count < numTimesheets) console.log("- Generated \t" + (timesheets.length + count) + "/" + numTimesheets)
+    }
 
-    client.close();
-                
-});
+    // Insert in the DB
+    console.log(chalk.cyan.bold("\n3. Insert in MongoDB:"))
+
+    var MongoClient = require('mongodb').MongoClient;
+
+    // Connect to the db
+    const mongodb_uri = process.env.DB_URI
+    const client = new MongoClient(mongodb_uri, { useUnifiedTopology: true } );
+
+    // do the insertions
+    try {
+        await client.connect();
+        const db = client.db("data_generation");
+        const collection = db.collection('timesheets')
+
+        for (const outputPath of outputFiles) {
+            console.log("- Reading file: " + outputPath)
+            var rawdataOutput = fs.readFileSync(outputPath);
+            var timesheetBatch = JSON.parse(rawdataOutput);
+            await collection.insertMany(timesheetBatch);    
+
+            var count = await collection.countDocuments()
+            console.log(chalk.yellow('* Inserted rows: ' + count+ "/" + numTimesheets));
+        };
+
+    } finally {
+        client.close();                
+    }
+
+    console.log(chalk.green.bold("\nDONE, script finished successfully!!\n"))
+}
+
+run().catch(console.dir);
