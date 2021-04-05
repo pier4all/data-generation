027d41d6c117bfc6ed6e2338a1fa97494c9caa53
espona <lucia.espona@gmail.com>
2021-04-05 22:16:09 +0200

    improved customers insertion script

--- insert/customers.js
+++ insert/customers.js
@@ -29,8 +29,6 @@ exports.run = async (numDocuments = 20) => {
     console.log(chalk.cyan.bold("\n2. Generating documents:"))
 
     // TODO: Guess all this params from numDocuments (original 35000)
-    const minDates = Math.min(10, numDocuments)	  // minimum amount of documents per input combination
-    const maxDates = Math.min(200, numDocuments)    // minimum amount of documents per input combination
     const batchSize = (numDocuments>500) ? Math.max(1, numDocuments/10) : numDocuments // number of documents to store in each JSON file 
 
     var document; 
@@ -42,79 +40,67 @@ exports.run = async (numDocuments = 20) => {
     var count = 0
     var outputFiles = []
 
-   
-    function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min) + min); //The maximum is exclusive and the minimum is inclusive
-    }
-
     // do the insertions
     try {
 
-        while ((count + documents.length) < numDocuments) { 
+        while ((count + documents.length) < numDocuments) {              
+
+            // added for customers
+            var name = faker.company.companyName();
+            var email = name.toLowerCase().replace(' and ', '_').replace(' ','_').replace(/[^a-zA-Z0-9_]/g,'') + '@' + faker.internet.email().split('@')[1];
+            
+            const language = ["DE", "EN", "FR"];
+            var random_language = language[Math.floor(Math.random() * language.length)];
 
             
-            var numDates = getRandomInt(minDates, maxDates)
-            for (let l = 0; l < numDates; l++) {              
+            document = { 
+                name: name,
+                email: email,
+                "language": random_language
+            }    
 
-                // added for customers
-                var name = faker.company.companyName();
-                var email = faker.internet.email();
-              
-                const language = ["DE", "EN", "FR"];
-                var random_language = language[Math.floor(Math.random() * language.length)];
+            document_export = { 
+                name: name,
+                email: email,
+                "language": random_language
+            }    
 
-                
-                document = { 
-                    name: name,
-                    email: email,
-                    "language": random_language
-                }    
+            documents.push(document)
+            documents_export.push(document_export)
 
-                document_export = { 
-                    name: name,
-                    email: email,
-                    "language": random_language
-                }    
+            // save one batch to file and db, end the process if enough time sheets
+            if ((documents.length >= batchSize) || ((count + documents.length) >= numDocuments)){
+                count += documents.length
 
-                documents.push(document)
-                documents_export.push(document_export)
+                var outputPath = path.join(__dirname, '..', 'data', 'output', 'customer_' + count + '.json');
+                fs.writeFileSync(outputPath, JSON.stringify(documents_export, null, 2))
+                outputFiles.push(outputPath)
+                documents_export = []
+                console.log(chalk.yellow.italic("* Saved batch to: " + outputPath))
 
-                // save one batch to file and db, end the process if enough time sheets
-                if ((documents.length >= batchSize) || ((count + documents.length) >= numDocuments)){
-                    count += documents.length
+                console.log(chalk.yellow.italic("* Inserting batch to MongoDB: " + DB + '.' + COLLECTION))
 
-                    var outputPath = path.join(__dirname, '..', 'data', 'output', 'customer_' + count + '.json');
-                    fs.writeFileSync(outputPath, JSON.stringify(documents_export, null, 2))
-                    outputFiles.push(outputPath)
-                    documents_export = []
-                    console.log(chalk.yellow.italic("* Saved batch to: " + outputPath))
+                // Timer
+                var start = process.hrtime();
+                console.log(chalk.magenta("\t - Timing insertMany of " + documents.length + " documents in MongoDB...")); // timer start
+
+                await collection.insertMany(documents)
+                documents = []
+
+                // stopping time and loging to console
+                var end = process.hrtime(start);
+                console.log(chalk.magenta.bold(`\t\t ... execution time: ${end[0]}s ${end[1] / 1000000}ms\n`));
 
 
-                    // Timer
-                    var start = process.hrtime();
-                    console.log(chalk.bgRed.bold("\nTiming insertMany of " + numDocuments + " documents in MongoDB...")); // timer start
-
-                    await collection.insertMany(documents)
-                    documents = []
-
-                    // stopping time and loging to console
-                    var end = process.hrtime(start);
-                    console.log(chalk.bgRed.bold(`\nExecution time: ${end[0]}s ${end[1] / 1000000}ms\n`));
-
-                    console.log(chalk.yellow.italic("* Inserted batch to MongoDB: " + DB + '.' + COLLECTION))
-
-                    //exit loop if enough total documents
-                    if (count >= numDocuments) break
-                }
+                //exit loop if enough total documents
+                if (count >= numDocuments) break
+                else console.log("- Generated \t" + (documents.length + count) + "/" + numDocuments)
             }
-            if (count < numDocuments) console.log("- Generated \t" + (documents.length + count) + "/" + numDocuments)
         }
 
     } finally {
         client.close();                
     }
 
-    console.log(chalk.green.bold("\n Insertion script finished successfully!!\n"))
+    console.log(chalk.green.bold("\n Insertion script for '" + COLLECTION + "' finished successfully!!\n"))
 }
