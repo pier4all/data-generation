8180cec7ecf91f4734d02380e39953bab3b2ed50
espona <lucia.espona@gmail.com>
2021-04-06 19:34:28 +0200

    Added operations to test perforance

--- insert/custom/use-api.js
+++ insert/custom/use-api.js
@@ -9,21 +9,32 @@ const { ObjectID } = require('mongodb');
 'use strict';
 const fs = require('fs');
 
-const generateRequest = (collection) => {
+const generateRequest = (collection, id) => {
     var url = `${process.env.API_URL}/crud/${collection}`
-    return url
+
+    if (id) return url + `/${id}`
+    else return url
 }
+
+async function wait(ms) {
+    return new Promise(resolve => {
+      setTimeout(resolve, ms);
+    });
+  }
 
 exports.run = async () => {
 
     var fileList = [
-        path.join(__dirname, '..', '..', 'data', 'input', 'customer.json'),
-        path.join(__dirname, '..', '..', 'data', 'input', 'employee.json'),
-        path.join(__dirname, '..', '..', 'data', 'input', 'project-edited.json'),
-        path.join(__dirname, '..', '..', 'data', 'input', 'service.json'),
-        path.join(__dirname, '..', '..', 'data', 'output', 'timesheet_100.json'),
-        path.join(__dirname, '..', '..', 'data', 'output', 'timesheet_200.json'),
-        path.join(__dirname, '..', '..', 'data', 'output', 'timesheet_300.json')    
+        //path.join(__dirname, '..', '..', 'data', 'input', 'customer.json'),
+        //path.join(__dirname, '..', '..', 'data', 'output', 'customer_500.json'),
+        path.join(__dirname, '..', '..', 'data', 'output', 'customer_test_1000.json')//,
+        //path.join(__dirname, '..', '..', 'data', 'output', 'customer.json')//,
+        // path.join(__dirname, '..', '..', 'data', 'input', 'employee.json'),
+        // path.join(__dirname, '..', '..', 'data', 'input', 'project-edited.json'),
+        // path.join(__dirname, '..', '..', 'data', 'input', 'service.json'),
+        // path.join(__dirname, '..', '..', 'data', 'output', 'timesheet_100.json'),
+        // path.join(__dirname, '..', '..', 'data', 'output', 'timesheet_200.json'),
+        // path.join(__dirname, '..', '..', 'data', 'output', 'timesheet_300.json')    
     ]
 
     for (var file of fileList) {
@@ -33,9 +44,11 @@ exports.run = async () => {
         let documents = JSON.parse(rawdata);
         console.log("\t - Read " + documents.length + " documents");
 
+        var inserted = []
+
         // insert them
         var collection = path.basename(file).split('.')[0].split('-')[0].split('_')[0]
-        console.log("\t - Collection '" + collection + "'");
+        console.log("\t - Inserting at Collection '" + collection + "'");
         var url = generateRequest(collection)
         for(var document of documents) {
             try {
@@ -49,11 +62,67 @@ exports.run = async () => {
                         }
                     })
                 }); 
-                var response = await axios.post( url, document);            
+                var response = await axios.post(url, document);     
+                inserted.push(response.data) 
+
             } catch (error) {
                 console.error(chalk.redBright.bold(error.message));
                 return
             }           
         }
+        
+        await wait(2000)
+
+        console.log("\t - Updating at Collection '" + collection + "'");
+        for(var document of inserted) {
+            try {
+                // TODO for other types of documents
+                var updated_document = { name: document.name + ' GmBH'}
+                url = generateRequest(collection, document._id)
+                var response = await axios.patch(url, updated_document);     
+            } catch (error) {
+                console.error(chalk.redBright.bold(error.message));
+                return
+            }           
+        }
+
+        await wait(2000)
+
+        console.log("\t - Querying current version at Collection '" + collection + "'");
+        for(var document of inserted) {
+            try {
+                url = generateRequest(collection, document._id)
+                var response = await axios.get(url);     
+            } catch (error) {
+                console.error(chalk.redBright.bold(error.message));
+                return
+            }           
+        }
+        await wait(2000)
+
+        console.log("\t - Querying past version at Collection '" + collection + "'");
+        for(var document of inserted) {
+            try {
+                url = generateRequest(collection, document._id)
+                var response = await axios.get(url + "/1");     
+            } catch (error) {
+                console.error(chalk.redBright.bold(error.message));
+                return
+            }           
+        }
+
+        await wait(2000)
+
+        console.log("\t - Deleting from Collection '" + collection + "'");
+        for(var document of inserted) {
+            try {
+                url = generateRequest(collection, document._id)
+                var response = await axios.delete(url);     
+            } catch (error) {
+                console.error(chalk.redBright.bold(error.message));
+                return
+            }           
+        }
+
     } 
 }
